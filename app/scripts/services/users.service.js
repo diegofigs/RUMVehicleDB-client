5e2e419377c9e351c02f896cf12a44f8e5f84e1c
Diego Figueroa <diego.fv94@gmail.com>
2017-02-14 21:30:05 -0400

    extract hardcoded routes to variables for easier changes

--- app/scripts/services/users.service.js
+++ app/scripts/services/users.service.js
@@ -7,11 +7,12 @@ window.appVersion = 2.0;
 angular.module('MaterialApp')
   .factory('UsersService', function($http, $log, AuthService) {
     var baseDomain = 'http://67.205.161.165/api/v1';
+    var resource = '/users';
     var user = {
       users: []
     };
     user.getUsers = function() {
-      user.users = $http.get(baseDomain + '/users')
+      user.users = $http.get(baseDomain + resource)
         .then(function(response) {
           user.users = response.data.data;
           return response.data.data;
@@ -22,13 +23,13 @@ angular.module('MaterialApp')
       return user.users;
     };
     user.createUser = function(user) {
-      return $http.post(baseDomain + '/users', user)
+      return $http.post(baseDomain + resource, user)
         .catch(function(error) {
           $log.log(error);
         });
     };
     user.deleteUser = function(user) {
-      return $http.delete(baseDomain + '/users/' + user.id, {
+      return $http.delete(baseDomain + resource + '/' + user.id, {
         headers: {
           Authorization: 'Bearer ' + AuthService.getToken()
         }
@@ -37,7 +38,7 @@ angular.module('MaterialApp')
         });
     };
     user.editUser = function(user) {
-      return $http.put(baseDomain + '/users/' + user.id, user, {
+      return $http.put(baseDomain + resource + '/' + user.id, user, {
         headers: {
           Authorization: 'Bearer ' + AuthService.getToken()
         }
