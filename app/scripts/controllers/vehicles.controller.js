72ae0dce7f83bc37bb76321a269de642785de61c
Diego Figueroa <diego.fv94@gmail.com>
2017-02-26 19:29:30 -0400

    initial refactor of Vehicles module - renamed files using function based convention - implement and design add vehicle functionality - refactor existing vehicle grid

---
+++ app/scripts/controllers/vehicles.controller.js
@@ -0,0 +1,91 @@
+/**
+ * Created by diegofigs on 1/31/17.
+ */
+'use strict';
+
+window.appVersion = 2.0;
+angular.module('MaterialApp')
+  .config(function($stateProvider) {
+    $stateProvider
+      .state('vehicles', {
+        url: '/vehicles',
+        parent: 'dashboard',
+        templateUrl: 'views/pages/dashboard' +
+        '/vehicles/vehicles.html?v=' + window.appVersion,
+        controller: 'VehiclesCtrl',
+        resolve: {
+          vehicles: function(VehiclesService) {
+            return VehiclesService.getVehicles();
+          }
+        }
+      })
+      .state('vehicles.add', {
+        url: '/vehicles/add',
+        parent: 'dashboard',
+        templateUrl: 'views/pages/dashboard' +
+        '/vehicles/add-vehicle.html?v=' + window.appVersion,
+        controller: 'VehiclesCtrl',
+        resolve: {
+          departments: function(DepartmentsService) {
+            return DepartmentsService.getDepartments();
+          }
+        }
+      })
+      .state('vehicles.view', {
+        url: '/vehicles/:id',
+        parent: 'dashboard',
+        templateUrl: 'views/pages/dashboard' +
+        '/vehicles/view-vehicle.html?v=' + window.appVersion,
+        controller: 'VehiclesCtrl',
+        resolve: {
+          vehicle: function($stateParams, VehiclesService) {
+            return VehiclesService.getVehicle($stateParams.id);
+          }
+        }
+      })
+      .state('vehicles.edit', {
+        url: '/vehicles/:id/edit',
+        parent: 'dashboard',
+        templateUrl: 'views/pages/dashboard' +
+        '/vehicles/edit-vehicle.html?v=' + window.appVersion,
+        controller: 'VehiclesCtrl',
+        resolve: {
+          vehicle: function($stateParams, VehiclesService) {
+            return VehiclesService.getVehicle($stateParams.id);
+          },
+          departments: function(DepartmentsService) {
+            return DepartmentsService.getDepartments();
+          }
+        }
+      });
+  })
+  .controller('VehiclesCtrl',
+    function($scope, $state, $log,
+             AuthService, DepartmentsService, VehiclesService) {
+      $scope.vehicles = VehiclesService.vehicles;
+      $scope.vehicle = VehiclesService.vehicle;
+      $scope.departments = DepartmentsService.departments;
+      $scope.newVehicle = {};
+      $scope.createVehicle = function() {
+        $scope.newVehicle.custodian_id = AuthService.getUser().id;
+        $log.log($scope.newVehicle);
+        return VehiclesService.createVehicle($scope.newVehicle)
+          .then(function(response) {
+          $log.log(response);
+          $state.go('vehicles');
+        });
+      };
+      $scope.deleteVehicle = function(vehicle) {
+        return VehiclesService.deleteVehicle(vehicle).then(function(response) {
+          $log.log(response);
+          $state.reload();
+        });
+      };
+      $scope.editCard = function() {
+        return VehiclesService.editVehicle($scope.vehicle)
+          .then(function(response) {
+          $log.log(response);
+          $state.go('vehicles');
+        });
+      };
+    });
---
+++ app/scripts/services/vehicles.service.js
@@ -0,0 +1,69 @@
+'use strict';
+
+window.appVersion = 2.0;
+angular.module('MaterialApp')
+  .factory('VehiclesService', function($http, $log, AuthService) {
+    var baseDomain = 'http://67.205.175.113/api/v1';
+    var resource = '/vehicles';
+    var vehicle = {
+      vehicles: [],
+      vehicle: {}
+    };
+    vehicle.getVehicles = function() {
+      vehicle.vehicles = $http.get(baseDomain + resource, {
+        headers: {
+          Authorization: 'Bearer ' + AuthService.getToken()
+        }
+      }).then(function(response) {
+        vehicle.vehicles = response.data.data;
+        $log.log(vehicle.vehicles);
+        return response.data.data;
+      })
+        .catch(function(error) {
+          $log.log(error);
+        });
+      return vehicle.vehicles;
+    };
+    vehicle.getVehicle = function(id) {
+      return $http.get(baseDomain + resource + '/' + id, {
+        headers: {
+          Authorization: 'Bearer ' + AuthService.getToken()
+        }
+      }).then(function(response) {
+        $log.log(response.data.data);
+        vehicle.vehicle = response.data.data;
+        return response.data.data;
+      })
+        .catch(function(error) {
+          $log.log(error);
+        });
+    };
+    vehicle.createVehicle = function(vehicle) {
+      return $http.post(baseDomain + resource, vehicle, {
+        headers: {
+          Authorization: 'Bearer ' + AuthService.getToken()
+        }
+      }).catch(function(error) {
+        $log.log(error);
+      });
+    };
+    vehicle.deleteVehicle = function(vehicle) {
+      return $http.delete(baseDomain + resource + '/' + vehicle.id, {
+        headers: {
+          Authorization: 'Bearer ' + AuthService.getToken()
+        }
+      }).catch(function(error) {
+        $log.log(error);
+      });
+    };
+    vehicle.editVehicle = function(vehicle) {
+      return $http.put(baseDomain + resource + '/' + vehicle.id, vehicle, {
+        headers: {
+          Authorization: 'Bearer ' + AuthService.getToken()
+        }
+      }).catch(function(error) {
+        $log.log(error);
+      });
+    };
+    return vehicle;
+  });
