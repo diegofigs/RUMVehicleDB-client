d6d17c50bd98b6474a8d09e704100df8c89280ee
Diego Figueroa <diego.fv94@gmail.com>
2017-02-26 21:40:08 -0400

    add moment.js dependency

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -39,6 +39,7 @@ module.exports = function(config) {
       '../bower_components/angular-translate-loader-static-files/angular-translate-loader-static-files.js',
       '../bower_components/ng-material-floating-button/src/mfb-directive.js',
       '../bower_components/ngstorage/ngStorage.js',
+      '../bower_components/moment/moment.js',
       '../bower_components/angular-mocks/angular-mocks.js',
       // endbower
       '../app/scripts/**/*.js',
