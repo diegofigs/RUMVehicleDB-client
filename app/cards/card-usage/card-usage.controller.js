83249e5ce7bc82d2089b8b99d25128aeda55fe46
janvega1 <jan.vega1@upr.edu>
2017-11-30 11:18:19 -0400

    Add markup along with assets for card usage view - include card usage controller - add directive for file upload

---
+++ app/cards/card-usage/card-usage.controller.js
@@ -0,0 +1,85 @@
+/**
+ * Created by Jan on 6/19/17.
+ */
+import moment from 'moment';
+
+export default class CardsUsageController {
+  /** @ngInject */
+  constructor($state, $log, FileUploader,
+              AuthService, CardsService, CardUsageService) {
+    this.$state = $state;
+    this.$log = $log;
+    this.authService = AuthService;
+    this.cardUsageService = CardUsageService;
+
+    this.cardsService = CardsService;
+    this.cardUsages = this.cardUsageService.cardUsages;
+    this.temp_date = new Date();
+
+    this.newCardUsage = {
+      date: '',
+      vehicle_mileage: '',
+      provider_number: '',
+      purchase_type: '',
+      total_liters: '',
+      total_receipt: '',
+      receipt_number: '',
+      comments: '',
+      vehicle_id: '',
+      card_id: '',
+      custodian_id: '',
+    };
+    this.onDateChange();
+
+    this.uploader = new FileUploader({
+      url: 'http://dev.uprm.edu/rumvehicles/api/v1/records',
+      alias: 'filename',
+      method: 'POST',
+      headers: {
+        Authorization: 'Bearer ' + this.authService.getToken(),
+      },
+    });
+
+    this.uploader.onBeforeUploadItem = (fileItem) => {
+      this.newCardUsage.custodian_id = this.authService.getUser().id;
+      this.newCardUsage.card_id = this.cardsService.card.id;
+      this.newCardUsage.vehicle_id = 1;
+      this.$log.log(this.newCardUsage);
+      fileItem.formData.push(this.newCardUsage);
+    };
+
+    this.uploader.onCompleteItem = (item, response, status, headers) => {
+      this.$log.log(item);
+      this.$log.log(status);
+      this.$log.log(response);
+      this.$state.go('dashboard.cards.view.card-usage');
+    };
+
+  }
+
+  onDateChange() {
+    this.newCardUsage.date = moment(this.temp_date).format('YYYY-M-D');
+  }
+
+  getCardUsages() {
+    //let cardID = this.cardsService.card.id;    Do I really need ID?
+    return this.cardUsageService.getCardUsages();
+
+  }
+
+  deleteCardUsage(id) {
+    return this.cardUsageService.deleteCardUsage(id)
+      .then(() => {
+        this.$state.reload();
+      });
+  }
+
+  editCardUsage(id) {
+    this.$log.log('I am inside editCardUsageEntry() in card-usage.controller');
+    return this.cardUsageService.editCardUsage(this.cardUsages.id)
+      .then(() => {
+        this.$state.go('dashboard.cards.view.card-usage');
+      });
+  }
+
+}
