9074068b72ad63b7808958d1efb3e27d691124c5
janvega1 <jan.vega1@upr.edu>
2017-11-30 11:16:25 -0400

    Add angular-file-upload to project and import in app

--- app/app.js
+++ app/app.js
@@ -9,6 +9,7 @@ import angularUiRouter from 'angular-ui-router';
 import angularMaterial from 'angular-material';
 import translate from 'angular-translate';
 import ngStorage from 'ngstorage';
+import 'angular-file-upload';
 import 'angular-truncate-2/src/angular-truncate-2';
 import 'angular-translate-loader-static-files';
 import 'angular-bootstrap-lightbox';
@@ -20,33 +21,36 @@ import cardsModule from './cards/cards';
 import departmentsModule from './departments/departments';
 import vehiclesModule from './vehicles/vehicles';
 import usersModule from './users/users';
+import cardUsageModule from './cards/card-usage/card-usage';
 
 export const appModule = 'app';
 
 /** @ngInject */
 angular.module(appModule, [
-    angularUiRouter,
-    angularAnimate,
-    angularMaterial,
-    ngStorage.name,
-    translate,
-    'ui.sortable',
-    'ng-mfb',
-    'truncate',
-    coreModule,
-    cardsModule,
-    departmentsModule,
-    vehiclesModule,
-    usersModule,
-  ]).config(($translateProvider) => {
-      $translateProvider.useStaticFilesLoader({
-          prefix: 'languages/',
-          suffix: '.json',
-        });
-      $translateProvider.useSanitizeValueStrategy(null);
-      $translateProvider.preferredLanguage('en');
-    })
-    .config(($mdIconProvider) => {
-      $mdIconProvider
-        .fontSet('mdi', 'material-icons');
-    });
+  angularUiRouter,
+  angularAnimate,
+  'angularFileUpload',
+  angularMaterial,
+  ngStorage.name,
+  translate,
+  'ui.sortable',
+  'ng-mfb',
+  'truncate',
+  coreModule,
+  cardsModule,
+  departmentsModule,
+  vehiclesModule,
+  usersModule,
+  cardUsageModule,
+]).config(($translateProvider) => {
+  $translateProvider.useStaticFilesLoader({
+    prefix: 'languages/',
+    suffix: '.json',
+  });
+  $translateProvider.useSanitizeValueStrategy(null);
+  $translateProvider.preferredLanguage('en');
+})
+  .config(($mdIconProvider) => {
+    $mdIconProvider
+      .fontSet('mdi', 'material-icons');
+  });
