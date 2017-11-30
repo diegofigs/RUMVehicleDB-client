e204f6374aefb28a504720d54cb20484e051460d
janvega1 <jan.vega1@upr.edu>
2017-11-30 11:18:19 -0400

    Add markup along with assets for card usage view - include card usage controller - add directive for file upload

---
+++ app/cards/card-usage/card-usage.directive.js
@@ -0,0 +1,42 @@
+/**
+ * Created by Jan on 6/23/17.
+ */
+
+
+class fileModel {
+
+  constructor($parse) {
+    this.link = this.linkFunc;
+    this.restrict = 'A';
+  }
+
+  linkFunc(scope, element, attrs) {
+    let model = $parse(attrs.fileModel);
+    let modelSetter = model.assign;
+
+    element.bind('change', function(){
+      scope.$apply(function(){
+        modelSetter(scope, element[0].files[0]);
+      });
+    });
+  }
+}
+
+
+var myApp = angular.module('myApp', []);
+
+myApp.directive('fileModel', ['$parse', function ($parse) {
+  return {
+    restrict: 'A',
+    link: function(scope, element, attrs) {
+      var model = $parse(attrs.fileModel);
+      var modelSetter = model.assign;
+
+      element.bind('change', function(){
+        scope.$apply(function(){
+          modelSetter(scope, element[0].files[0]);
+        });
+      });
+    }
+  };
+}]);
