f4211639c6c1c32d6b662317b9fb50fbe546303e
Diego Figueroa <diego.fv94@gmail.com>
2017-12-13 10:59:08 -0400

    add all reconciliation related views to records module

--- app/records/records.js
+++ app/records/records.js
@@ -4,6 +4,9 @@ import baseTemplate from './views/conc-base.html';
 import step1template from './views/conc-step1-invoice.html';
 import step2template from './views/conc-step2-results.html';
 import step3template from './views/conc-step3-custodians.html';
+import baseTemplateReconc from './views/reconciliation-base.html';
+import step1templateReconc from './views/reconciliation-step1-justify.html';
+import step2templateReconc from './views/reconciliation-step2-review.html';
 
 /** @ngInject **/
 const recordsModule = angular.module('app.records', [])
@@ -30,6 +33,22 @@ const recordsModule = angular.module('app.records', [])
       .state('dashboard.conciliation.step3', {
         url: '/step3',
         template: step3template,
+        controller: 'RecordsCtrl as ctrl',
+      })
+      .state('dashboard.reconciliation', {
+        abstract: true,
+        url: '/reconciliation',
+        template: baseTemplateReconc,
+        controller: 'RecordsCtrl as ctrl',
+      })
+      .state('dashboard.reconciliation.step1', {
+        url: '/step1',
+        template: step1templateReconc,
+        controller: 'RecordsCtrl as ctrl',
+      })
+      .state('dashboard.reconciliation.step2', {
+        url: '/step2',
+        template: step2templateReconc,
         controller: 'RecordsCtrl as ctrl',
       });
   }).name;
