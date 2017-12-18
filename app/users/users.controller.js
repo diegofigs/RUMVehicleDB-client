6f0a0f4aea695b3e5e0217248e30175f655c2830
janvega1 <jan.vega1@upr.edu>
2017-12-18 12:58:32 -0400

    fix user add/edit fjunctionality to include new fields, add user delete confirmation

--- app/users/users.controller.js
+++ app/users/users.controller.js
@@ -3,19 +3,54 @@
  */
 
 export default class UsersController {
-  /** @ngInject */
-  constructor($state, $log, UsersService) {
+  constructor($state, $log, UsersService, DepartmentsService, swal) {
     this.$state = $state;
     this.$log = $log;
     this.usersService = UsersService;
 
     this.users = this.usersService.users;
+    this.user = this.usersService.user;
+    this.newUser = {};
+    this.departments = DepartmentsService.departments;
+
+    this.swal = swal;
   }
 
   createUser() {
-    return this.usersService.createUser(this.user)
+    return this.usersService.createUser(this.newUser)
       .then(() => {
       this.$state.go('dashboard.users.list');
+    });
+  }
+
+  confirmUserDeletion(user){
+
+    this.swal({
+      title: 'Do you really want to delete this user?',
+      type: 'warning',
+      confirmButtonText: 'Confirm',
+      confirmButtonColor: '#4caf50',
+      showCancelButton: 'true',
+      cancelButtonText: 'Cancel',
+      cancelButtonColor: '#f44336',
+    }).then(() => {
+
+      this.deleteUser(user)
+        .then(() => {
+
+          this.swal({
+            title: 'User has been deleted',
+            type: 'success'
+          });
+
+          this.$state.reload();
+        })
+        .catch((error) => {
+          this.swal({
+            title: 'Error: User could not be deleted',
+            type: 'error'
+          });
+        });
     });
   }
 
@@ -25,8 +60,8 @@ export default class UsersController {
     });
   }
 
-  editUser(user) {
-    return this.usersService.editUser(user).then(() => {
+  editUser() {
+    return this.usersService.editUser(this.user).then(() => {
       this.$state.go('dashboard.users.list');
     });
   }
--- app/users/users.js
+++ app/users/users.js
@@ -9,7 +9,6 @@ import usersTemplate from '../users/users.html';
 import usersAddTemplate from '../users/views/add-user.html';
 import usersEditTemplate from '../users/views/edit-user.html';
 
-/** @ngInject */
 const usersModule = angular.module('app.users', [])
   .config(function($stateProvider) {
     $stateProvider
@@ -31,12 +30,22 @@ const usersModule = angular.module('app.users', [])
       .state('dashboard.users.add', {
         url: '/add',
         template: usersAddTemplate,
-        controller: 'UsersCtrl as ctrl'
+        controller: 'UsersCtrl as ctrl',
+        resolve: {
+          departments: (DepartmentsService) =>
+            DepartmentsService.getDepartments(),
+        }
       })
       .state('dashboard.users.edit', {
         url: '/:id/edit',
         template: usersEditTemplate,
-        controller: 'UsersCtrl as ctrl'
+        controller: 'UsersCtrl as ctrl',
+        resolve: {
+          user: ($stateParams, UsersService) =>
+            UsersService.getUser($stateParams.id),
+          departments: (DepartmentsService) =>
+            DepartmentsService.getDepartments()
+        },
       });
   })
   .service('UsersService', usersService)
