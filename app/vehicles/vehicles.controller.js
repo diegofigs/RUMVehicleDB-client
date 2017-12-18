a986cd3c25df94b0b0ef43265d12352fc330c7d4
janvega1 <jan.vega1@upr.edu>
2017-12-18 12:54:04 -0400

    add vehicle types to markup, fix problem wehn adding vehicle caused by date format

--- app/vehicles/vehicles.controller.js
+++ app/vehicles/vehicles.controller.js
@@ -1,15 +1,17 @@
 /**
  * Created by diegofigs on 1/31/17.
  */
+
+import moment from 'moment-es6';
+
 export default class VehiclesController {
-  /** @ngInject */
   constructor($state, $log,
-           AuthService, DepartmentsService, UsersService, VehiclesService) {
+           AuthService, DepartmentsService, UsersService, VehiclesService, swal) {
     this.$state = $state;
     this.$log = $log;
     this.authService = AuthService;
     this.departmentsService = DepartmentsService;
-    this.usersService = UsersService
+    this.usersService = UsersService;
     this.vehiclesService = VehiclesService;
 
     this.vehicles = this.vehiclesService.vehicles;
@@ -17,8 +19,11 @@ export default class VehiclesController {
     this.departments = this.departmentsService.departments;
     //Get all custodians for card filtering purposes.
     this.custodianNames = this.usersService.users;
+    this.vehicleTypes = this.vehiclesService.vehicleTypes;
     this.newVehicle = {};
+    this.swal = swal;
 
+    // Filter object will contain filter parameters for filtering vehicles
     this.filter = {
       department_id: '',
       custodian_id: '',
@@ -28,17 +33,57 @@ export default class VehiclesController {
 
   createVehicle() {
     this.newVehicle.custodian_id = this.authService.getUser().id;
+
+    this.newVehicle.marbete_date = moment(this.newVehicle.marbete_date).format('YYYY-MM-DD');
+    this.newVehicle.decomission_date = moment(this.newVehicle.decomission_date).format('YYYY-MM-DD');
+    this.newVehicle.inscription_date = moment(this.newVehicle.inscription_date).format('YYYY-MM-DD');
+    this.newVehicle.inspection_date = moment(this.newVehicle.inspection_date).format('YYYY-MM-DD');
+
     return this.vehiclesService.createVehicle(this.newVehicle)
       .then(() => {
       this.$state.go('dashboard.vehicles.list');
       });
   }
 
-  deleteVehicle(vehicle) {
-    return this.vehiclesService.deleteVehicle(vehicle)
-      .then(() => {
-      this.$state.reload();
+  /**
+   * Shows confirmation dialog to user
+   * If user confirms, vehicle will be deleted (if no error in deletion is present)
+   * If user cancels, he/she will stay in the "Vehicles" view
+   */
+  confirmVehicleDeletion(vehicle) {
+
+    this.swal({
+      title: 'Do you really want to delete this vehicle?',
+      type: 'warning',
+      confirmButtonText: 'Confirm',
+      confirmButtonColor: '#4caf50',
+      showCancelButton: 'true',
+      cancelButtonText: 'Cancel',
+      cancelButtonColor: '#f44336',
+    }).then(() => {
+
+      this.deleteVehicle(vehicle)
+        .then(() => {
+
+          this.swal({
+            title: 'Vehicle has been deleted',
+            type: 'success'
+          });
+
+          this.$state.reload();
+      })
+        .catch((error) => {
+          this.swal({
+            title: 'Error: Vehicle could not be deleted',
+            type: 'error'
+          });
+        });
     });
+  }
+
+
+  deleteVehicle(vehicle) {
+    return this.vehiclesService.deleteVehicle(vehicle);
   }
 
   editVehicle() {
@@ -54,7 +99,7 @@ export default class VehiclesController {
 
   applyVehicleFilter() {
     return this.vehiclesService.getVehicles(this.filter)
-      .then( () => {
+      .then(() => {
         this.vehicles = this.vehiclesService.vehicles;
       });
   }
