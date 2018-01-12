921b73a5c87bb41188f0d412be7865756a84cba9
Diego Figueroa <diego.fv94@gmail.com>
2018-01-12 10:58:23 -0400

    optimize app module - move translations from asset to importing - remove .min extensions for improved compat

--- app/app.js
+++ app/app.js
@@ -1,29 +1,25 @@
 import 'angular-material/angular-material.scss';
-import 'angular-material-data-table/dist/md-data-table.min.css';
-import 'ng-material-floating-button/mfb/dist/mfb.min.css';
-import 'mdi/css/materialdesignicons.min.css';
-import 'sweetalert2/dist/sweetalert2.min.css';
-import 'angular-bootstrap-lightbox/dist/angular-bootstrap-lightbox.min.css'
-
+import 'angular-material-data-table/dist/md-data-table.css';
+import 'ng-material-floating-button/mfb/dist/mfb.css';
+import 'mdi/css/materialdesignicons.css';
+import 'sweetalert2/dist/sweetalert2.css';
 import './styles/app-green.scss';
-import './styles/scss/widgets/card-filter.scss'
 
 import angular from 'angular';
 import angularAnimate from 'angular-animate';
 import angularUiRouter from 'angular-ui-router';
-import 'angular-ui-router/release/stateEvents.js'
+import 'angular-ui-router/release/stateEvents.js';
 import angularMaterial from 'angular-material';
-import translate from 'angular-translate/dist/angular-translate.min';
-import ngStorage from 'ngstorage/ngStorage.min';
+import translate from 'angular-translate';
+import ngStorage from 'ngstorage';
 import { ngSweetAlert2 } from 'angular-h-sweetalert';
 import 'angular-file-upload';
-import 'angular-truncate-2/dist/angular-truncate-2.min';
-import 'angular-translate-loader-static-files/angular-translate-loader-static-files.min';
-import 'angular-bootstrap-lightbox/dist/angular-bootstrap-lightbox.min';
+import 'angular-truncate-2';
+import 'angular-translate-loader-static-files';
 import 'angular-ui-sortable/dist/sortable.min';
 import 'ng-material-floating-button/src/mfb-directive';
 import 'material-angular-paging/build/dist.min';
-import 'angular-material-data-table'
+import 'angular-material-data-table';
 import 'angular-messages';
 
 import coreModule from './core/core';
@@ -33,6 +29,9 @@ import vehiclesModule from './vehicles/vehicles';
 import usersModule from './users/users';
 import cardUsageModule from './cards/card-usage/card-usage';
 import recordsModule from './records/records';
+
+import englishTranslations from './languages/en.json';
+import spanishTranslations from './languages/es.json';
 
 export const appModule = 'app';
 
@@ -59,10 +58,8 @@ angular.module(appModule, [
   cardUsageModule,
   recordsModule,
 ]).config(($translateProvider) => {
-  $translateProvider.useStaticFilesLoader({
-    prefix: 'languages/',
-    suffix: '.json',
-  });
+  $translateProvider.translations('en', englishTranslations);
+  $translateProvider.translations('es', spanishTranslations);
   $translateProvider.useSanitizeValueStrategy(null);
   $translateProvider.preferredLanguage('en');
 })
@@ -83,15 +80,15 @@ angular.module(appModule, [
     $mdThemingProvider.theme('default')
       .primaryPalette('newGreen')
       .warnPalette('newRed');
-  }).run(['$rootScope', '$state',function($rootScope){
-
+  })
+  .run(($rootScope) => {
     $rootScope.stateIsLoading = false;
 
-    $rootScope.$on('$stateChangeStart',function(){
+    $rootScope.$on('$stateChangeStart',() => {
       $rootScope.stateIsLoading = true;
-  });
-    $rootScope.$on('$stateChangeSuccess',function(){
-      $rootScope.stateIsLoading = false;
-  });
+    });
 
-}]);
+    $rootScope.$on('$stateChangeSuccess', () => {
+      $rootScope.stateIsLoading = false;
+    });
+  });
