aa59458c99fc521a03d762b18cd466e240054f79
Diego Figueroa <diego.fv94@gmail.com>
2018-01-17 19:18:45 -0400

    retain notification count depending on user type

--- app/core/dashboard/services/notifications.service.js
+++ app/core/dashboard/services/notifications.service.js
@@ -1,7 +1,8 @@
 export default class NotificationService {
-  constructor($http, $log, API) {
+  constructor($http, $log, AuthService, API) {
     this.$http = $http;
     this.$log = $log;
+    this.authService = AuthService;
     this.API = API;
     this.resource = '/api/v1/dashboard/notifications/';
   }
@@ -11,7 +12,13 @@ export default class NotificationService {
       .then((response) => {
         this.$log.log(response);
         this.notifications = response.data.notifications;
-        this.unread_notifications_count = response.data.notifications.unread_notifications_count;
+        if(this.authService.getUser().user_type_name === 'admin'){
+          this.justified_notifications_count = response.data.justified_notifications_count;
+        }
+        else{
+          this.unread_notifications_count = response.data.unread_notifications_count;
+        }
+        return this.notifications;
       });
   }
 
